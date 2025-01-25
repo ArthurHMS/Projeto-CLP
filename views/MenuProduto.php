4df7bf467df0d85a13f5b060150aaff971ca3e7b
ArthurHMS <ms.arthurhenrique@gmail.com>
2025-01-25 20:43:12 -0300

    consertei a view

---
+++ views/MenuProduto.php
@@ -0,0 +1,70 @@
+<?php
+
+require_once 'MenuEntidade.php';
+require_once 'DAOProduto.php';
+require_once 'Produto.php';
+
+class MenuProduto extends MenuEntidade {
+    private $dao;
+
+    public function __construct() {
+        parent::__construct();
+        $this->dao = DAOProduto::getInstance();
+    }
+
+    protected function mostrarTitulo() {
+        echo "MENU PRODUTOS\n";
+    }
+
+    protected function listar() {
+        echo $this->dao->__toString();
+    }
+
+    protected function adicionar($scanner) {
+        $nome = null;
+        $valor = 0.0;
+
+        while (true) {
+            try {
+                echo "\nDigite o nome: ";
+                $nome = trim(fgets($scanner));
+
+                echo "Digite o valor: ";
+                $valor = floatval(trim(fgets($scanner)));
+
+                if ($nome == null || $nome === "" || $valor <= 0.0) {
+                    throw new Exception("\nFavor informar os dados corretamente.\n");
+                } else {
+                    break;
+                }
+            } catch (Exception $ex) {
+                echo $ex->getMessage();
+            }
+        }
+
+        $this->dao->adicionar(new Produto($nome, $valor));
+    }
+
+    protected function remover($scanner) {
+        $nome = null;
+
+        while (true) {
+            try {
+                echo "\nDigite o nome: ";
+                $nome = trim(fgets($scanner));
+
+                if ($nome == null || $nome === "") {
+                    throw new Exception("\nFavor informar o nome corretamente.\n");
+                } else {
+                    break;
+                }
+            } catch (Exception $ex) {
+                echo $ex->getMessage();
+            }
+        }
+
+        $this->dao->removerPorNome($nome);
+    }
+}
+
+?>
---
+++ views/MenuVenda.php
@@ -0,0 +1,85 @@
+<?php
+
+require_once 'MenuEntidade.php';
+require_once 'DAOVenda.php';
+require_once 'DAOProduto.php';
+require_once 'Venda.php';
+require_once 'Produto.php';
+
+class MenuVenda extends MenuEntidade {
+    private $daoVenda;
+    private $daoProduto;
+
+    public function __construct() {
+        parent::__construct();
+        $this->daoVenda = DAOVenda::getInstance();
+        $this->daoProduto = DAOProduto::getInstance();
+    }
+
+    protected function mostrarTitulo() {
+        echo "MENU VENDAS\n";
+    }
+
+    protected function listar() {
+        echo $this->daoVenda->__toString();
+    }
+
+    protected function adicionar($scanner) {
+        $venda = new Venda();
+        $produto = null;
+        $qtd = 0;
+
+        while (true) {
+            while (true) {
+                try {
+                    echo "\nDigite o nome do produto: ";
+                    $produtoNome = trim(fgets($scanner));
+                    $produto = $this->daoProduto->buscarPorNome($produtoNome);
+
+                    echo "Digite a quantidade: ";
+                    $qtd = intval(trim(fgets($scanner)));
+
+                    if ($produto == null || $qtd <= 0) {
+                        throw new Exception("\nFavor informar os dados corretamente.\n");
+                    } else {
+                        break;
+                    }
+                } catch (Exception $ex) {
+                    echo $ex->getMessage();
+                }
+            }
+
+            $venda->adicionarItem($produto, $qtd);
+
+            echo "\nDeseja adicionar outro produto à venda (1-SIM/0-NAO)? ";
+            if (intval(trim(fgets($scanner))) != 1) {
+                break;
+            }
+        }
+
+        echo "\n\nNOTA FISCAL\n" . $venda->__toString();
+        $this->daoVenda->adicionar($venda);
+    }
+
+    protected function remover($scanner) {
+        $id = 0;
+
+        while (true) {
+            try {
+                echo "\nDigite o id: ";
+                $id = intval(trim(fgets($scanner)));
+
+                if ($id <= 0) {
+                    throw new Exception("\nFavor informar os dados corretamente.\n");
+                } else {
+                    break;
+                }
+            } catch (Exception $ex) {
+                echo $ex->getMessage();
+            }
+        }
+        $this->daoVenda->remover($id);
+    }
+}
+
+?>
